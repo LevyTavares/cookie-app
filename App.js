baaa29e2e7950db7ba8955364917abd39d676c9a
Seu Nome <seu-email@exemplo.com>
2025-11-17 19:52:40 -0300

    Refactor code structure for improved readability and maintainability

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import react, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 class App extends Component {
   render() {
-    return{
-      <View >
-
+    return (
+      <View style={styles.container}>
+        <Text>CU</Text>
+        <Image source={require('./src/biscoito.png')} style={styles.img}/>
+        <Text>Terceito olho Inexplorado</Text>
       </View>
-    }
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+
+  img: {
+    width: 250,
+    height: 250,
+  },
+});
+
+export default App;
